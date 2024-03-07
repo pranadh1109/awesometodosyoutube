c46ae69658fcca68e951b9fb25303551c916d3df
pranadh1109 <162546730+pranadh1109@users.noreply.github.com>
2024-03-07 12:04:18 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/";
+const uri = process.env.MONGODB_URI || mongodb+srv://245120733071:<Pranadh@7677>@cluster0.rowp1p7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
 
 const options = {
     serverApi: {
